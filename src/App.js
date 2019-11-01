da2d9fe5eab85d3ae2ef96305deda5a9e01318d7
vtekade <vtekade@vmware.com>
2019-11-01 15:19:55 +0530

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop/hats' component={HatsPage}/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// import React from 'react';
+// import { Route, Link } from 'react-router-dom';
+
+// import './App.css';
+
+// const HomePage = props => {
+//   console.log(props.history);
+//   return (
+//     <div>
+//       <button onClick={() => props.history.push('/topics')}>Topics </button>
+//       <h1>HOME PAGE</h1>
+//     </div>
+//   );
+// };
+
+// const TopicsList = props => {
+//   console.log(props.history);
+//   return (
+//     <div>
+//       <h1>TOPIC LIST PAGE</h1>
+//       <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link><div/>
+//       <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link><div/>
+//       <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+//     </div>
+//   );
+// };
+
+// const TopicDetail = props => {
+//   console.log(props.history);
+//   return (
+//     <div>
+//       <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+//     </div>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <div>
+//       <Route exact path='/' component={HomePage} />
+//       <Route exact path='/topics' component={TopicsList} />
+//       <Route exact path='/blog/asdqw/topics' component={TopicsList} />
+//       <Route path='/topics/:topicId' component={TopicDetail} />
+//       <Route exact path='/blog/topics' component={TopicsList} />
+//       <Route path='/blog/topics/:topicId' component={TopicDetail} />
+//     </div>
+//   );
+// }
+
+// export default App;
